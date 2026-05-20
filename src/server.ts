b626cc60b97f71a558aefb4717afeac2c815af52
Shahriar Ahmed <shafishahriar98@gmail.com>
2026-05-21 00:52:12 +0600

    second

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,26 @@
-import express from "express"
-const app = express()
-const port = 5000
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+const app: Application = express();
+const port = 5000;
 
-app.get('/user', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req: Request, res: Response) => {
+  //   res.send('Hello World!')
+  res.status(200).json({
+    message: "express server",
+  });
+});
+
+app.post("/", async (req: Request, res: Response) => {
+  console.log(req.body);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
