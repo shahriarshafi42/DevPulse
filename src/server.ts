da4ee44f9eb9b41111a3a1d2f5ddefb30352838c
Shahriar Ahmed <shafishahriar98@gmail.com>
2026-05-21 18:28:05 +0600

    Added issues CRUD operations

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,6 @@ const app: Application = express();
 const port = 5000;
 
 app.use(express.json());
-app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 
 const pool = new Pool({
@@ -18,17 +17,35 @@ const pool = new Pool({
 const initDB = async () => {
   try {
     await pool.query(`
-        CREATE TABLE IF NOT EXISTS users(
-        id SERIAL PRIMARY KEY ,
-        name VARCHAR(20),
-        email VARCHAR(20) UNIQUE NOT NULL,
-        password VARCHAR(20) NOT NULL,
+     CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(45),
+      email VARCHAR(100) NOT NULL UNIQUE,
+      password VARCHAR(255) NOT NULL,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+    );
+  `);
+    await pool.query(`
+    ALTER TABLE users
+    ADD COLUMN IF NOT EXISTS role VARCHAR(20) DEFAULT 'contributor'
+    CHECK (role IN ('contributor', 'maintainer'));
+`);
 
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
+    await pool.query(`
+          CREATE TABLE IF NOT EXISTS issues(
+          id SERIAL PRIMARY KEY,
+          reporter_id INT REFERENCES users(id)  ON DELETE CASCADE,
+          
+          title VARCHAR(255) NOT NULL,
+          description TEXT,
+          type VARCHAR(50),
+          status VARCHAR(30) DEFAULT 'open',
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP	
+          )
+          `);
 
-        ) 
-        `);
     console.log("database connected succesfully");
   } catch (error) {
     console.log(error);
@@ -37,13 +54,12 @@ const initDB = async () => {
 initDB();
 
 app.get("/", (req: Request, res: Response) => {
-  //   res.send('Hello World!')
   res.status(200).json({
     message: "express server",
   });
 });
-
-app.post("/", async (req: Request, res: Response) => {
+//user
+app.post("/api/auth/signup", async (req: Request, res: Response) => {
   console.log(req.body);
   const { name, email, password } = req.body;
   try {
@@ -56,7 +72,8 @@ app.post("/", async (req: Request, res: Response) => {
     console.log(result);
 
     res.status(201).json({
-      message: "user created succesfully",
+      success: true,
+      message: "User registered successfully",
       data: result.rows[0],
     });
   } catch (error: any) {
@@ -66,7 +83,86 @@ app.post("/", async (req: Request, res: Response) => {
     });
   }
 });
+// issuues
+app.post("/api/issues", async (req: Request, res: Response) => {
+  console.log(req.body);
+  const { title, description, type, status, reporter_id } = req.body;
+  try {
+    const result = await pool.query(
+      `   INSERT INTO issues(title, description, type,status,reporter_id) VALUES ($1,$2,$3,$4,$5)
+    RETURNING *
+    `,
+      [title, description, type, status, reporter_id],
+    );
+    console.log(result);
 
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      succes: false,
+      message: error.message,
+      data: error,
+    });
+  }
+});
+
+app.get("/api/issues", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`
+    SELECT * FROM issues
+    `);
+    res.status(200).json({
+      success: true,
+      message: "Retrieve all issues successfully",
+      data: result.rows,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      succes: false,
+      message: error.message,
+      data: error,
+    });
+  }
+});
+
+app.get("/api/issues/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  console.log("id:", id);
+
+  try {
+    const result = await pool.query(
+      `
+      SELECT * FROM issues WHERE id = $1
+      `,
+      [id]
+    );
+    if (result.rows.length ===0) {
+      res.status(404).json({
+      success: false,
+      message: "issues not found",
+      data: {},
+    });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Retrieve single issue successfully",
+      data: result.rows[0],
+    });
+
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: error,
+    });
+  }
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
