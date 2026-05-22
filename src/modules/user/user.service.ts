15bedbd0f19c70285579f10977d812cb5fada0ec
Shahriar Ahmed <shafishahriar98@gmail.com>
2026-05-22 12:26:12 +0600

    Implemented password hashing with bcryptjs

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,16 +1,21 @@
 import { pool } from "../../db";
+import bcrypt from "bcryptjs";
 
 const creatUerDB = async (payload: any) => {
 
 
     
   const { name, email, password } = payload;
+
+  const hashPassword = await bcrypt.hash(password,10)
   const result = await pool.query(
     `   INSERT INTO users(name, email,password) VALUES ($1,$2,$3)
     RETURNING *
     `,
-    [name, email, password],
+    [name, email, hashPassword],
+  
   );
+  delete result.rows[0].password
   return result;
 };
 export const userService = {
